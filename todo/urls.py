44005c9e3bdeb2aacb4ff412f59f57454606cd6e
tarik <63872091+tariksitki@users.noreply.github.com>
2022-08-30 17:08:53 +0200

    delete and update completed

--- todo/urls.py
+++ todo/urls.py
@@ -1,10 +1,12 @@
 
 from django.urls import path
 
-from todo.views import home, todo_add
+from todo.views import home, todo_add, todo_delete, todo_update
 
 urlpatterns = [
     path("", home, name="home"),
     path("add/", todo_add, name="add"),
+    path("update/<int:id>/", todo_update, name="update"),
+    path("delete/<int:id>/", todo_delete, name="delete"),
 
 ]
--- todo/views.py
+++ todo/views.py
@@ -40,3 +40,51 @@ def todo_add(request):
                                      ## icine de url deki name yazilir.
 
     return render(request, "todo/todo_add.html", context)
+
+
+
+
+
+
+
+    #####   update:
+    ## update etmek icin spesific bir todo ya ihtiyac var. bunun icin id kullanilir
+
+def todo_update(request, id):
+    todo = Todo.objects.get(id=id)
+    form = TodoForm(instance=todo)
+        #### Önemli: ilk etap da formun ici bos olmaz. instance olur. eger method get den post a dönerse, bu durumda birde request girer icine 
+    
+    if request.method == "POST":
+        form = TodoForm(request.POST, instance=todo) ## create den tek farki instance
+        if form.is_valid():
+            form.save()
+            return redirect("home")
+
+    context = {
+        "form" : form,
+        "todo" : todo
+    }
+
+    return render(request, "todo/todo_update.html", context)
+
+
+
+
+
+
+    #### delete:
+    ### Önemli:  delete isleminin template inde form kullanilir ama view in de olmaz.
+    ### burada form.delete denmez, todo.delete denir.
+
+def todo_delete(request, id):
+    todo = Todo.objects.get(id=id)
+    
+    if request.method == "POST":
+        todo.delete()
+        return redirect("home")
+    context = {
+        "todo" : todo
+    }
+
+    return render(request, "todo/todo_delete.html", context)
